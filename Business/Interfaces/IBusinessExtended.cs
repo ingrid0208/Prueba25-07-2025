d0503bcef0b38f849bb05de3ef7534cb4ffe6158
ingrid0208 <yulissamain.04@gmail.com>
2025-10-29 10:38:10 -0500

    subiendo correcciones

--- Business/Interfaces/IBusinessExtended.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Business.Interfaces
-{
-    public interface IBusinessExtended<TDtoGet, TDto> : IBusiness<TDto>
-    {
-        Task<IEnumerable<TDtoGet>> GeAllJoin();
-        Task<TDtoGet?> GetByIdJoin(int id);
-        Task<IEnumerable<string>> GetAllRolUser(int idUser);
-    }
-}
